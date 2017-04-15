2da7884921dfd91a3c1e0911a3edd428a749e18c
Tim Brown <timothy.brown@lincoln.oxon.org>
2017-04-15 23:02:49 +0100

    Init

---
+++ PN9.cs
@@ -0,0 +1,26 @@
+﻿namespace PN9
+{
+    class PN9
+    {
+        public PN9() { State = 511; }
+        public int State { get; private set; }
+
+        private void NextBit()
+        {
+            var bit = Get6() ^ Get1();
+            State /= 2;
+            State |= 256 * bit;
+        }
+        public byte GetNext()
+        {
+            var b = (byte)State;
+            for (int i = 0; i < 8; i++)
+                NextBit();
+
+            return b;
+        }
+
+        private byte Get1() { return (byte)(State & 1); }
+        private byte Get6() { return (byte)((State & 32) / 32); }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace PN9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pn9 = new PN9();
+            var series = Enumerable.Range(1, 511)
+                        .Select(i => pn9.GetNext())
+                        .ToArray();
+
+            var output = series.Aggregate("", (acc,el) => $"{acc}{bits(el, 8)}  {el}\n");
+            Console.Write(output);
+            Console.ReadKey();
+        }
+
+        static string bits(int b, int length = 8)
+        {
+            return new string(Enumerable.Range(0, length)
+                                  .Select(i => ((b >> i) & 1) == 0 ? '0' : '1')
+                                  .Reverse()
+                                  .ToArray());
+        }
+    }
+}
